a1285728d3d352166b8fe6df27aa078d9373d22e
Andrew Lee <alee14498@protonmail.com>
2020-10-06 13:01:38 +0000

    stuff

---
+++ pages/page1.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Où est le chef de la paye?</h1>
+    <!-- Reveal animation here -->
+    <p>C'est Xi Jinping.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
---
+++ pages/page2.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>La langues de Chine</h1>
+    <!-- Reveal animation here -->
+    <p>Voici le langues Chine avais.</p>
+    <ul>
+      <li>Mandarin</li>
+      <li>Wu</li>
+      <li>Anglais</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+}
+</script>
