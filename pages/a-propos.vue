7c8607b4bae2ec9e07e460e31a01ca654f192aaa
Andrew Lee <alee14498@protonmail.com>
2020-10-15 12:35:03 +0000

    Added license

--- pages/a-propos.vue
+++ pages/a-propos.vue
@@ -4,6 +4,27 @@
     <p>Cette site web est créé pour mon Français projet.</p>
     <h1>Crédits</h1>
     <p>Site web créé par <a href="https://alee14.me">Andrew Lee</a></p>
+    <h1>License</h1>
+    <p>
+      Chine Projet<br />
+      Copyright (C) 2020 Andrew Lee <br />
+      <br />
+      This program is free software: you can redistribute it and/or modify
+      <br />
+      it under the terms of the GNU General Public License as published by
+      <br />
+      the Free Software Foundation, either version 3 of the License, or <br />
+      (at your option) any later version. <br />
+      <br />
+      This program is distributed in the hope that it will be useful, <br />
+      but WITHOUT ANY WARRANTY; without even the implied warranty of <br />
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the <br />
+      GNU General Public License for more details. <br />
+      <br />
+      You should have received a copy of the GNU General Public License <br />
+      along with this program. If not, see
+      <a href="https://www.gnu.org/licenses">https://www.gnu.org/licenses</a>.
+    </p>
   </div>
 </template>
 
